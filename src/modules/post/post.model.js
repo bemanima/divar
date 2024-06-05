0da3a56b26319e014217f088f0b3ea747872d09d
bemanima <bemanima@gmail.com>
2024-06-05 17:24:12 +0330

    Add post model

---
+++ src/modules/post/post.model.js
@@ -0,0 +1,19 @@
+const { Schema, Types } = require("mongoose");
+
+const PostSchema = new Schema(
+  {
+    title: { type: String, required: true },
+    content: { type: String, required: true },
+    category: { type: Types.ObjectId, ref: "Category", required: true },
+    province: { type: String, required: true },
+    city: { type: String, required: true },
+    district: { type: String, required: true },
+    coordinate: { type: [Number], required: true },
+    images: { type: [String], required: false, default: [] },
+  },
+  { timestamps: true }
+);
+
+const PostModel = model("Post", PostSchema);
+
+module.exports = PostModel;
